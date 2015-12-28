ca8672919f1e95db17c796805a536dfb45b44388
Maver <maver@Mac-Pro-de-Maver.local>
2015-12-27 20:45:11 -0600

    3 de 4 condiciones(2,3,4) falta la 1

--- holaGit.playground/section-1.swift
+++ holaGit.playground/section-1.swift
@@ -2,6 +2,24 @@
 
 import UIKit
 
-var miNombre = "Adrian"
-print(miNombre)
-
+for var i = 0; i<=100; i++
+{
+    
+    var par = (i%2)
+    
+    //condicion par
+    if par == 0{
+        print("\(i)par\n")
+    }
+    //condicion impar
+    else{
+        print("\(i)impar\n")
+    }
+    
+    //rango de 30 a 40
+    if i >= 30 && i <= 40
+    {
+        print("\(i)Viva Swift!!!\n")
+    }
+    
+}
